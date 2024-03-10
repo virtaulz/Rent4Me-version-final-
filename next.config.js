9c9fd2af7a5be9027dfc27b05af4224f8059f160
virtualzz <e2048889@cmaisonneuve.qc.ca>
2024-03-09 21:33:01 -0500

    maj

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   experimental: {
   },
+   webpack: (config) => {
+       config.externals = [...config.externals, 'bcrypt'];
+      return config;
+    },
   images: {
     domains: [
       'res.cloudinary.com', 
