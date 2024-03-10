36d17b1a7b884993a653af0a7b9a49d5b8146ff7
virtualzz <e2048889@cmaisonneuve.qc.ca>
2024-03-09 22:20:08 -0500

    maj

--- app/component/navbar/Logo.tsx
+++ app/component/navbar/Logo.tsx
@@ -1,17 +1,15 @@
 'use client';
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 
 const Logo = () => {
     const router = useRouter();
 
     return (
-        <Link href="/">
-            <a>
-                <img onClick={() => router.push('/')} src="Capture.png" alt="Logo" className="hidden md:block cursor-pointer" height="90" width="250" />
-            </a>
-        </Link>
+        <div onClick={() => router.push('/')}>
+            <Image src="/images/capture.png" alt="Logo" className="hidden md:block cursor-pointer" height={100} width={250} />
+        </div>
     );
 }
 
