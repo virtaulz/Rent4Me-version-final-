ae3cfd5f393655d631a10252350e7e622cf86fcf
virtualzz <e2048889@cmaisonneuve.qc.ca>
2024-03-09 20:59:58 -0500

    maj

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
-/** 
- * @type {import('next').NextConfig} 
- */
+// next.config.mjs
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.html$/,
+        loader: 'ignore-loader', // Ignorer le chargement des fichiers HTML
+      });
+    }
+
+    return config;
+  },
   images: {
     domains: [
       'res.cloudinary.com', 
